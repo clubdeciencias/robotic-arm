2be28af3ac35fd2427c29ff8e4cb5e0d3501eae0
Alberto Zugazagoitia <alberto@zugazagoitia.com>
2018-07-25 14:55:57 +0200

    Fixed

---
+++ servo/servo/servo.ino
@@ -0,0 +1,243 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+#include <ArduinoJson-v6.2.3-beta.h> //JSON parsing library
+
+Servo myservo1;
+Servo myservo2;
+Servo myservo3;
+Servo myservo4;
+Servo myservo5;
+
+int OutServo1 = 5;
+int OutServo2 = 6;
+int OutServo3 = 9;
+int OutServo4 = 10;
+int OutServo5 = 11; // YET TO DECIDE A PIN
+
+int vel = 0;
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	myservo1.attach(OutServo1);
+	myservo2.attach(OutServo2);
+	myservo3.attach(OutServo3);
+	myservo4.attach(OutServo4);
+	myservo5.attach(OutServo5);
+
+	Serial.begin(9600);
+
+	pinMode(12, OUTPUT);
+	pinMode(13, OUTPUT);
+
+	const size_t bufferSize = JSON_OBJECT_SIZE(3) + 40;
+	DynamicJsonBuffer jsonBuffer(bufferSize);
+}
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+	if (Serial.available()) {
+		const char* json = = Serial.read(); //read JSON input and store it
+		JsonObject& root = jsonBuffer.parseObject(json); //Parse JSON
+		char servo = root["servo"]; // "servo" char
+		char direction = root["direction"]; // "direction" char
+		int angle = root["angle"]; // "angle" int
+		rotate(servo, direction, angle); //execute rotation with the given data. 
+		
+	}
+}
+
+void led(int timeInMiliseconds) { // method to have a LED on for a determinated time in miliseconds
+	digitalWrite(12, HIGH);
+	delay(timeInMiliseconds);
+	digitalWrite(12, LOW);
+}
+
+void rotate(char servo, char direction, int angle) {
+	
+	int calculatedMicroseconds = ;//la función de la leche que convierte "int angle" a seconds teniendo en cuenta "char direction"
+	
+	switch (servo)
+	{
+	case '1': 
+		digitalWrite(13, HIGH);
+		digitalWrite(13, LOW);
+		myservo1.writeMicroseconds(calculatedMicroseconds);
+		break;
+
+	case '2':
+		digitalWrite(13, HIGH);
+		digitalWrite(13, LOW);
+		myservo2.writeMicroseconds(calculatedMicroseconds);
+		break;
+
+	case '3':
+		digitalWrite(13, HIGH);
+		digitalWrite(13, LOW);
+		myservo3.writeMicroseconds(calculatedMicroseconds);
+		break;
+
+	case '4':
+		digitalWrite(13, HIGH);
+		digitalWrite(13, LOW);
+		myservo4.writeMicroseconds(calculatedMicroseconds);
+		break;
+
+	case '5':
+		digitalWrite(13, HIGH);
+		digitalWrite(13, LOW);
+		myservo5.writeMicroseconds(calculatedMicroseconds);
+		break;
+
+	
+	default:
+		Serial.write("UNKNOWN SERVO ERROR");
+		for (int a = 0; a < 10; a++) {
+			led(100);
+			delay(100);
+		}
+	}
+}
+
+/*
+void giros() {
+	while (rotating) {
+		if (Serial.available()) {
+			delay(200);
+			servo = Serial.read();
+			digitalWrite(13, HIGH);
+			if (servo == '2')
+			{
+				digitalWrite(13, LOW);
+				myservo1.writeMicroseconds(1700);
+				servo = 'Z';
+			}
+			if (servo == '3')
+			{
+				myservo1.writeMicroseconds(1300);
+				digitalWrite(13, HIGH);
+				servo = 'Z';
+
+			}
+			if (servo == '1')
+			{
+				myservo1.writeMicroseconds(1500);
+				digitalWrite(13, LOW);
+				servo = 'Z';
+			}
+			if (servo == 'E') {
+				myservo1.writeMicroseconds(1500);
+				rotating = false;
+			}
+			else {
+				Serial.write("Error, introduce un comando válido. 1,2 o 3");
+			}
+		}
+	}
+}
+void giros2() {
+	while (rotating) {
+		if (Serial.available()) {
+			delay(200);
+			servo = Serial.read();
+			digitalWrite(13, HIGH);
+			if (servo == '2')
+			{
+				digitalWrite(13, LOW);
+				myservo2.writeMicroseconds(1700);
+				servo = 'Z';
+			}
+			if (servo == '3')
+			{
+				myservo2.writeMicroseconds(1300);
+				digitalWrite(13, HIGH);
+				servo = 'Z';
+
+			}
+			if (servo == '1')
+			{
+				myservo2.writeMicroseconds(1500);
+				digitalWrite(13, LOW);
+				servo = 'Z';
+			}
+			if (servo == 'E') {
+				myservo2.writeMicroseconds(1500);
+				rotating = false;
+			}
+			else {
+				Serial.write("Error, introduce un comando válido. 1,2 o 3");
+			}
+		}
+	}
+}
+void giros3() {
+	while (rotating) {
+		if (Serial.available()) {
+			delay(200);
+			servo = Serial.read();
+			digitalWrite(13, HIGH);
+			if (servo == '2')
+			{
+				digitalWrite(13, LOW);
+				myservo3.writeMicroseconds(1700);
+				servo = 'Z';
+			}
+			if (servo == '3')
+			{
+				myservo3.writeMicroseconds(1300);
+				digitalWrite(13, HIGH);
+				servo = 'Z';
+
+			}
+			if (servo == '1')
+			{
+				myservo3.writeMicroseconds(1500);
+				digitalWrite(13, LOW);
+				servo = 'Z';
+			}
+			if (servo == 'E') {
+				myservo3.writeMicroseconds(1500);
+				rotating = false;
+			}
+			else {
+				Serial.write("Error, introduce un comando válido. 1,2 o 3");
+			}
+		}
+	}
+}
+void giros4() {
+	while (rotating) {
+		if (Serial.available()) {
+			delay(200);
+			servo = Serial.read();
+			digitalWrite(13, HIGH);
+			if (servo == '2')
+			{
+				digitalWrite(13, LOW);
+				myservo4.writeMicroseconds(1700);
+				servo = 'Z';
+			}
+			if (servo == '3')
+			{
+				myservo4.writeMicroseconds(1300);
+				digitalWrite(13, HIGH);
+				servo = 'Z';
+
+			}
+			if (servo == '1')
+			{
+				myservo4.writeMicroseconds(1500);
+				digitalWrite(13, LOW);
+				servo = 'Z';
+			}
+			if (servo == 'E') {
+				myservo4.writeMicroseconds(1500);
+				rotating = false;
+			}
+			else {
+				Serial.write("Error, introduce un comando válido. 1,2 o 3");
+			}
+		}
+	}
+}
+*/
